acbd5b9672cfb0c8fee0b56cfcf5e12bdeebb710
Aditya <adityaexperimental@gmail.com>
2025-12-17 13:59:03 +0530

    Update customize.sh
    lets see

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -61,7 +61,7 @@ if [[ "${SOURCE_PRODUCT_SHIPPING_API_LEVEL:-}" != "${TARGET_PRODUCT_SHIPPING_API
 
     SMALI_PATCH "system" "system/framework/services.jar" \
         "smali/com/android/server/SystemServer.smali" "replace" \
-        "startOtherServices(Lcom/android/server/utils/TimingsTraceAndSlog;)V" \
+        ".method public final startOtherServices(Lcom/android/server/utils/TimingsTraceAndSlog;)V" \
         "MAINLINE_API_LEVEL: $SOURCE_PRODUCT_SHIPPING_API_LEVEL" \
         "MAINLINE_API_LEVEL: $TARGET_PRODUCT_SHIPPING_API_LEVEL"
 
